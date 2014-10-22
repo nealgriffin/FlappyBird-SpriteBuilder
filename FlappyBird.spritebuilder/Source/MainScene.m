f641fac96cfa7eef78019cb8c4a92e6f1122ab00
Cornelius Griffin <cornelius.griffin@gmail.com>
2014-10-22 16:12:49 -0400

    Automatically Committed for MakeGamesWithUs

--- FlappyBird.spritebuilder/Source/MainScene.m
+++ FlappyBird.spritebuilder/Source/MainScene.m
@@ -39,7 +39,7 @@
     
     _grounds = @[_ground1, _ground2];
     _clouds = @[_cloud1, _cloud2];
-    _bushes = @[_bush1, _bush1];
+    _bushes = @[_bush1, _bush2];
     
     for (CCNode *ground in _grounds) {
         // set collision txpe
