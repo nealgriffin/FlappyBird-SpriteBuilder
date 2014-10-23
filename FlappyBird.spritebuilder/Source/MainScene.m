fd7d679067b8ed084007ebbd1961a4174d845b62
Cornelius Griffin <cornelius.griffin@gmail.com>
2014-10-22 23:13:26 -0400

    Automatically Committed for MakeGamesWithUs

--- FlappyBird.spritebuilder/Source/MainScene.m
+++ FlappyBird.spritebuilder/Source/MainScene.m
@@ -59,6 +59,7 @@
     _grounds = @[_ground1, _ground2];
     _clouds = @[_cloud1, _cloud2];
     _bushes = @[_bush1, _bush2];
+    _parallaxBackground = [CCParallaxNode node];
     [_parallaxContainer addChild:_parallaxBackground];
     //Note that the brush ratio is larger than the cloud
     _bushParallaxRatio = ccp(0.9, 1);
