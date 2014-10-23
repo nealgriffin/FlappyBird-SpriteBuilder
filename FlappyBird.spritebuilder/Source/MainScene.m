d00d8159a7c6a874a3a753968d8b76ec7b459bf4
Cornelius Griffin <cornelius.griffin@gmail.com>
2014-10-22 23:16:50 -0400

    Automatically Committed for MakeGamesWithUs

--- FlappyBird.spritebuilder/Source/MainScene.m
+++ FlappyBird.spritebuilder/Source/MainScene.m
@@ -173,7 +173,7 @@
         [character.physicsBody applyAngularImpulse:-40000.f*delta];
     }
     
-    _parallaxBackground.position = ccp(_parallaxBackground.position.x - (character.physicsBody.velocity.x * delta), _parallaxBackground.position.y);
+    physicsNode.position = ccp(physicsNode.position.x - (character.physicsBody.velocity.x * delta), physicsNode.position.y);
     
     // loop the ground
     for (CCNode *ground in _grounds) {
